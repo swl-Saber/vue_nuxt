59944bb7d0d13784dba5c7a60afdc2b681296cca
Potter <shenwanlin2007@163.com>
2020-03-06 22:11:53 +0800

    element组件库转为中文格式

--- plugins/element-ui.js
+++ plugins/element-ui.js
@@ -2,4 +2,6 @@ import Vue from 'vue'
 import Element from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/en'
 
-Vue.use(Element, { locale })
+// Vue.use(Element, { locale })//国际化英文
+Vue.use(Element)//转为中文
+
