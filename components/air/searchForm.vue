0974483d2313bd680fe9b3fd557d0fc692b00e9c
Potter <shenwanlin2007@163.com>
2020-03-06 11:59:13 +0800

    基本数据绑定，使用死数据渲染建议列表

--- components/air/searchForm.vue
+++ components/air/searchForm.vue
@@ -2,32 +2,37 @@
   <div class="search-form">
     <!-- 头部切换 -->
     <el-row type="flex" class="search-tab">
-      <span v-for="(item,index) of tabs" :key="index" :class="{active:currentTab==index}" 
-      @click="changeTab(index)">
+      <span
+        v-for="(item,index) of tabs"
+        :key="index"
+        :class="{active:currentTab==index}"
+        @click="changeTab(index)"
+      >
         <i :class="item.icon"></i>
         {{item.name}}
       </span>
     </el-row>
     <!-- 搜索表单 -->
     <el-form :model="form" :rules="rules" ref="form" label-width="80px" class="search-form-content">
-  <el-form-item label="出发城市">
-      <!-- input输入框-带建议输入 -->
-    <el-autocomplete placeholder="请搜索出发城市" class="el-autocomplete"></el-autocomplete>
-  </el-form-item>
-  <el-form-item label="到达城市">
-    <el-autocomplete placeholder="请搜索到达城市" class="el-autocomplete"></el-autocomplete>
-  </el-form-item>
-  <el-form-item label="出发时间">
-      <!-- 日期选择器 -->
-        <el-date-picker
-        type="date"
-        placeholder="请选择日期" style="width: 100%;">
-        </el-date-picker>
-  </el-form-item>
-  <el-form-item>
-    <el-button type="primary" icon="el-icon-search" style="width:100%">搜索</el-button>
-  </el-form-item>
-</el-form>
+      <el-form-item label="出发城市">
+        <!-- input输入框-带建议输入 -->
+        <el-autocomplete v-model="form.departCity" placeholder="请搜索出发城市" class="el-autocomplete"  :fetch-suggestions="queryDepartSearch"></el-autocomplete>
+      </el-form-item>
+      <el-form-item label="到达城市">
+        <el-autocomplete v-model="form.arriveCity" placeholder="请搜索到达城市" class="el-autocomplete"  :fetch-suggestions="queryArriveSearch"></el-autocomplete>
+      </el-form-item>
+      <el-form-item label="出发时间">
+        <!-- 日期选择器 -->
+        <el-date-picker type="date" v-model="form.departDate" placeholder="请选择日期" style="width: 100%;"></el-date-picker>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="el-icon-search" style="width:100%">搜索</el-button>
+      </el-form-item>
+      <!-- 换 -->
+      <div class="change">
+        <span @click="handleChange">换</span>
+      </div>
+    </el-form>
   </div>
 </template>
 
@@ -46,14 +51,40 @@ export default {
           icon: "iconfont iconshuangxiang"
         }
       ],
-      form:{},
-      rules:{}
+      form: {
+        departCity:'',
+        arriveCity:'',
+        departDate:''
+      },
+      rules: {}
     };
   },
-  methods:{
-      changeTab(index){
-          this.currentTab=index;
-      }
+  methods: {
+    //tab切换时触发
+    changeTab(index) {
+      this.currentTab = index;
+    },
+    //出发城市输入框获得焦点时触发
+    queryDepartSearch(searchValue,showList){
+      //自带两个参数，第一个是当前输入值，第二个是返回数据并显示列表的回调
+      console.log('当前搜索值为'+searchValue);
+      const res=[
+        {
+          value:'上海',
+        },
+        {
+          value:'广州',
+        },
+        {
+          value:'深圳'
+        }
+      ]
+      showList(res)
+    },
+    //到达城市输入框获得焦点时触发
+    queryArriveSearch(){},
+    //出发城市和目标城市切换时触发
+    handleChange(){},
   }
 };
 </script>
@@ -92,12 +123,68 @@ export default {
     }
   }
 }
-.search-form-content{
-  padding:15px 50px 15px 15px;
+.search-form-content {
+  padding: 15px 50px 15px 15px;
   position: relative;
 
-  .el-autocomplete{
+  .el-autocomplete {
     width: 100%;
+  }
+  .change {
+    position: absolute;
+    top: 35px;
+    right: 15px;
+    &:after,
+    &:before {
+      display: block;
+      content: "";
+      position: absolute;
+      left: -35px;
+      width: 25px;
+      height: 1px;
+      background: #ccc;
+    }
+    &:after {
+      top: 0;
+    }
+
+    &:before {
+      top: 60px;
+    }
+    span {
+      display: block;
+      position: absolute;
+      top: 20px;
+      right: 0;
+      font-size: 12px;
+      background: #999;
+      color: #fff;
+      width: 20px;
+      height: 20px;
+      line-height: 18px;
+      text-align: center;
+      border-radius: 2px;
+      cursor: pointer;
+
+      &:after,
+      &:before {
+        display: block;
+        content: "";
+        position: absolute;
+        left: 10px;
+        width: 1px;
+        height: 20px;
+        background: #ccc;
+      }
+
+      &:after {
+        top: -20px;
+      }
+
+      &:before {
+        top: 20px;
+      }
+    }
   }
 }
 </style>
