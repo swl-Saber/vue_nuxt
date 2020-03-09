f67d1ce829d345980309178000f6cc0e10b7bee4
Potter <shenwanlin2007@163.com>
2020-03-09 17:15:29 +0800

    多个过滤器协作

--- components/air/flightsFilters.vue
+++ components/air/flightsFilters.vue
@@ -8,7 +8,7 @@
         2019-06-17
       </el-col>
       <el-col :span="4">
-        <el-select size="mini" v-model="airport" placeholder="起飞机场" @change="handleAirport">
+        <el-select size="mini" v-model="airport" placeholder="起飞机场" @change="handleFilters">
           <el-option
             v-for="(item,index) of totalData.options.airport"
             :key="index"
@@ -18,7 +18,7 @@
         </el-select>
       </el-col>
       <el-col :span="4">
-        <el-select size="mini" v-model="flightTimes" placeholder="起飞时间" @change="handleFlightTimes">
+        <el-select size="mini" v-model="flightTimes" placeholder="起飞时间" @change="handleFilters">
           <el-option
             v-for="(item,index) of totalData.options.flightTimes"
             :key="index"
@@ -28,7 +28,7 @@
         </el-select>
       </el-col>
       <el-col :span="4">
-        <el-select size="mini" v-model="company" placeholder="航空公司" @change="handleCompany">
+        <el-select size="mini" v-model="company" placeholder="航空公司" @change="handleFilters">
           <el-option
             v-for="(item,index) of totalData.options.company"
             :key="index"
@@ -38,7 +38,7 @@
         </el-select>
       </el-col>
       <el-col :span="4">
-        <el-select size="mini" v-model="airSize" placeholder="机型" @change="handleAirSize">
+        <el-select size="mini" v-model="airSize" placeholder="机型" @change="handleFilters">
           <el-option
             v-for="(item,index) of sizeOptions"
             :key="index"
@@ -73,40 +73,70 @@ export default {
   props: ["totalData"],
   methods: {
     // 选择机场时候触发
-    handleAirport() {
-      const newFliterData = this.totalData.flights.filter(flight => {
+    handleAirport(list) {
+      const newFliterData = list.filter(flight => {
         return flight.org_airport_name == this.airport;
       });
-      this.$emit("setFlightList", newFliterData);
+      // this.$emit("setFlightList", newFliterData);
+      return newFliterData;
     },
 
     // 选择出发时间时候触发
-    handleFlightTimes() {
+    handleFlightTimes(list) {
       const [from, to] = this.flightTimes.split(",");
-      const newFliterData = this.totalData.flights.filter(flight => {
+      const newFliterData = list.filter(flight => {
         const start = flight.dep_time.split(":")[0];
         return Number(from) <= Number(start) && Number(to) > Number(start);
       });
-      this.$emit("setFlightList", newFliterData);
+      return newFliterData;
+      // this.$emit("setFlightList", newFliterData);
     },
 
     // 选择航空公司时候触发
-    handleCompany() {
+    handleCompany(list) {
       //过滤出公司名相同的数据，使用自定义事件把过滤的数据传给父组件渲染
-      const newFliterData = this.totalData.flights.filter(flight => {
+      const newFliterData = list.filter(flight => {
         return flight.airline_name == this.company;
       });
-      this.$emit("setFlightList", newFliterData);
+      return newFliterData;
+      // this.$emit("setFlightList", newFliterData);
     },
 
     // 选择机型时候触发
-    handleAirSize() {
-      const newFliterData = this.totalData.flights.filter(flight => {
+    handleAirSize(list) {
+      const newFliterData = list.filter(flight => {
         return flight.plane_size == this.airSize;
       });
-      this.$emit("setFlightList", newFliterData);
+      return newFliterData;
+      // this.$emit("setFlightList", newFliterData);
     },
+    handleFilters(){
+      //原始数据
+      let newFliterData=this.totalData.flights;
+      // 四个函数调用需要进行判断是否有数据，否则筛选一个的时候，会同时触发四个函数，其他函数没有数据就会返回一个空数组
+      //1.通过机场过滤器
+      if(this.airport){
+        newFliterData=this.handleAirport(newFliterData);
+        // console.log(newFliterData);
+      }
+      //2.通过起飞时间过滤器
+      if(this.flightTimes){
+        newFliterData=this.handleFlightTimes(newFliterData);
+        // console.log(newFliterData);
+      }
+      //3.通过公司过滤器
+      if(this.company){
+        newFliterData=this.handleCompany(newFliterData);
+        // console.log(newFliterData);
+      }
+      //4.通过机型过滤器
+      if(this.airSize){
+        newFliterData=this.handleAirSize(newFliterData);
+        // console.log(newFliterData);
+      }
 
+      this.$emit('setFlightList',newFliterData);
+    },
     // 撤销条件时候触发
     handleFiltersCancel() {}
   }
