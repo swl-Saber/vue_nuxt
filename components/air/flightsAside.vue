507073447f781199ec75e77b26dffb264ac3ef82
Potter <shenwanlin2007@163.com>
2020-03-10 14:23:37 +0800

    历史记录在侧边栏渲染，并在路由带上参数

--- components/air/flightsAside.vue
+++ components/air/flightsAside.vue
@@ -20,11 +20,11 @@
 
     <div class="history">
       <h5>历史查询</h5>
-      <nuxt-link to="#">
+      <nuxt-link :to="`/air/flights?departCity=${item.departCity}&departCode=${item.departCode}&destCity=${item.destCity}&destCode=${item.destCode}&departDate=${item.departDate}`" v-for="(item,index) of $store.state.history.list" :key="index">
         <el-row type="flex" justify="space-between" align="middle" class="history-item">
           <div class="air-info">
-            <div class="to-from">广州 - 上海</div>
-            <p>2019-06-16</p>
+            <div class="to-from">{{item.departCity}} - {{item.destCity}}</div>
+            <p>{{item.departDate}}</p>
           </div>
           <span>选择</span>
         </el-row>
@@ -35,7 +35,12 @@
 
 
 <script>
-export default {};
+export default {
+  mounted(){
+    console.log(this.$store.state.history.list);
+    
+  }
+};
 </script>
 
 <style scoped lang="less">
