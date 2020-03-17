6a3c5621837012410e1f3ca67a8183cdad230fbf
Potter <shenwanlin2007@163.com>
2020-03-17 20:43:35 +0800

    子组件间的传值

--- components/hotel/breadcrumb.vue
+++ components/hotel/breadcrumb.vue
@@ -2,13 +2,15 @@
   <div class="breadcrumb">
     <el-breadcrumb separator-class="el-icon-arrow-right">
       <el-breadcrumb-item>酒店</el-breadcrumb-item>
-      <el-breadcrumb-item>酒店预订</el-breadcrumb-item>
+      <el-breadcrumb-item>{{cityName}}酒店预订</el-breadcrumb-item>
     </el-breadcrumb>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["cityName"]
+};
 </script>
 
 <style lang="less" scoped>
